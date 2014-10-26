b0683aff4c2e98268d5343f0967784016ac7a835
Brendan Wilson <brendan.x.wilson@gmail.com>
2014-10-26 16:32:17 -0500

    Fleshed out a few things in the maze class and added a position class to the maze.

--- maze.py
+++ maze.py
@@ -5,6 +5,13 @@ Author: Brendan Wilson
 
 from maze_constants import *
 
+opposite = {'north': 'south', 'east': 'west', 'south': 'north', \
+            'west': 'east'}
+
+class WallError(Exception):
+    def __init__(self, error):
+        self.error = error
+
 class Cell(object):
     """north, east, south, and west will be references to other Cells.
     This class will probably just be passed in to the drawing
@@ -12,11 +19,11 @@ class Cell(object):
 
     __slots__ = '_directions', '_visited', '_xLoc', '_yLoc'
 
-    case = {'north': 0, 'east': 1, 'south': 2, 'west': 3}
-    opposite = {'north': 'south', 'east': 'west', 'south': 'north', \
-                'west': 'east'}
 
-    def __init__(self, x, y, north=False, east=False, south=False, west=False):
+    case = {'north': 0, 'east': 1, 'south': 2, 'west': 3}
+
+    def __init__(self, x, y, \
+                 north=False, east=False, south=False, west=False):
         self._xLoc = x
         self._yLoc = y
         self._directions = [north, east, south, west]
@@ -31,16 +38,16 @@ class Cell(object):
     def visited(self):
         return self._visited
 
-    def set_direction(self, direction):
+    def open_wall(self, direction):
         """direction must be 'north', 'east', etc."""
         self._directions[Cell.case[direction]] = True
 
-    def get_direction(self, direction):
-        """See above"""
+    def is_open(self, direction):
+        """Checks whether the given direction is open"""
         return self._directions[Cell.case[direction]]
 
     def get_position(self):
-        """Return the x, y position of the top left corner as a tuple"""
+        """Return the x, y position of the cell as a tuple"""
         return self._xLoc, self._yLoc
 
     def get_links(self):
@@ -50,16 +57,75 @@ class Cell(object):
     def is_junction(self):
         return self._directions.count(True) > 2
 
+    def is_deadend(self):
+        return self._directions.count(True) == 1
+
+    def is_isolated(self):
+        """For determining if the cell has no connections"""
+        return self._directions.count(True) == 0
+
+    def is_hallway(self):
+        return self._directions.count(True) == 2
+
 class Maze(object):
 
-    def __init__(self):
-        self._cells = [[Cell(x, y) for y in xrange(0, MAZE_HEIGHT, CELL_SIZE)]\
-                        for x in xrange(0, MAZE_WIDTH, CELL_SIZE)]
+    class Position(object):
+        def __init__(self, maze, cell):
+            self.__maze = maze
+            self.__cell = cell
 
-    def get_cell(self, x, y):
+        def move(self, direction):
+            self.__cell = self.__maze._move(self.__cell, direction)
+
+        def _access_cell(self):
+            return self.__cell
+
+
+    movement = {'north': (lambda x, y: (x, y-1)),
+                'east': (lambda x, y: (x+1, y)),
+                'south': (lambda x, y: (x, y+1)),
+                'west': (lambda x, y: (x-1, y))}
+
+    def __init__(self):
+        self._cells = [[Cell(x, y) \
+                        for y in xrange(MAZE_HEIGHT/CELL_SIZE)] \
+                        for x in xrange(MAZE_WIDTH/CELL_SIZE)]
+
+    def _get_cell(self, x, y):
         """Returns the cell at position x, y.
         x and y are in terms of cell numbers, not pixels"""
-        return
+        return self._cells[x][y]
+
+    def _clip(self, cell, direction):
+        """Returns the cell in the given direction, regardless of walls.
+        Raises WallError if cell is out of bounds."""
+        currentX, currentY = cell.get_position()
+        newX, newY = Maze.movement[direction](currentX, currentY)
+        if newX < 0 or newX >= MAZE_WIDTH/CELL_SIZE \
+           or newY < 0 or newY >= MAZE_HEIGHT/CELL_SIZE:
+           raise WallError('Out of bounds')
+        return self._get_cell(newX, newY) 
+
+
+    def _join_cells(self, cell, direction):
+        """Opens the wall between the cell given and the one in the
+        given direction.
+        """
+        cell.open_wall(direction)
+        self._clip(cell, direction).open_wall(opposite[direction])
+        # The above line will return a WallError if an out of bounds cell
+        # is used
+
+    def _move(self, cell, direction):
+        """Movement with wall checking"""
+        if cell.is_open(direction):
+            return self._clip(cell, direction)
+        else:
+            raise WallError('There is a wall there')
+
+    def start(self):
+        return Position(self, self._get_cell(0, 0))
 
 if __name__ == '__main__':
     maze = Maze()
+    

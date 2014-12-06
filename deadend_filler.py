a298b47753a033cba2051a76f37f0f6848e0fcd7
Brendan Wilson <brendan.x.wilson@gmail.com>
2014-12-05 22:05:20 -0600

    Added license statement to source code

--- deadend_filler.py
+++ deadend_filler.py
@@ -1,7 +1,22 @@
 """
 A maze solver that searches for dead ends in the maze and fills them in.
 The only thing left at the end should be the solution
-Author: Brendan Wilson
+
+Copyright (C) 2014 Brendan Wilson
+brendan.x.wilson@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from maze_constants import *
